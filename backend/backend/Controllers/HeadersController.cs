1102828e09ee0c331ddf5cdaa28289df72fcae04
Verner Fortelius <verner@fortelius.net>
2021-11-27 14:20:44 +0200

    switch to net6

---
+++ backend/backend/Controllers/HeadersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeadersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHeaders()
+        {
+            var headers = HttpContext.Request.Headers;
+            return Ok(headers);
+        }
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,26 +1,96 @@
+using backend.Hubs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders.Physical;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.IO;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
-namespace backend
+namespace backend;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        builder.Services.AddCors();
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SchemaFilter<NullableSchemaFilter>();
+            c.SupportNonNullableReferenceTypes();
+            c.UseAllOfToExtendReferenceSchemas();
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend", Version = "v1" });
+        });
+
+        builder.Services.AddSingleton<MockMessageService>();
+        builder.Services.AddSignalR().AddAzureSignalR(builder.Configuration["SignalRConnectionString"]);
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.Authority = builder.Configuration["Auth0:Authority"];
+            options.Audience = builder.Configuration["Auth0:Audience"];
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                NameClaimType = ClaimTypes.NameIdentifier,
+            };
+        });
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
+
+        app.UseCors(o => o.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.UseStaticFiles();
+        app.UseCsrfValidationMiddleware();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHub<TestHub>("/hubs/test");
+        });
+
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.Run(async context =>
+        {
+            var cspNonce = WebEncoders.Base64UrlEncode(RandomNumberGenerator.GetBytes(32));
+            var csp = builder.Configuration["CspPolicy"].Replace("{cspNonce}", cspNonce);
+            context.Response.Headers.Add("Content-Security-Policy", csp);
+            context.Response.Cookies.Append("csp-nonce", cspNonce, new CookieOptions { IsEssential = true, SameSite = SameSiteMode.Strict, Secure = true });
+
+            var csrfToken = WebEncoders.Base64UrlEncode(RandomNumberGenerator.GetBytes(32));
+            context.Response.Cookies.Append("XSRF-TOKEN", csrfToken, new CookieOptions { IsEssential = true, SameSite = SameSiteMode.Strict, Secure = true });
+
+            context.Response.ContentType = "text/html";
+            await context.Response.SendFileAsync(new PhysicalFileInfo(new FileInfo(Path.Combine(builder.Environment.WebRootPath, "index.html"))));
+            await context.Response.CompleteAsync();
+        });
+
+        app.Run();
     }
 }
