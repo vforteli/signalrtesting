41cce9bdcc1b9ee7e4b01020d85fb98278e55317
Verner Fortelius <verner@fortelius.net>
2021-12-06 15:47:20 +0200

    dont load messages

--- src/Components/Messages/MessageContainer.tsx
+++ src/Components/Messages/MessageContainer.tsx
@@ -7,26 +7,22 @@ import { RootState } from "../..";
 import { getNotificationEnabled, setNotificationEnabled } from "../../store/app/appSlice";
 import { FormGroup, FormControlLabel, Switch, Box, Button } from "@mui/material";
 import { HubConnectionState } from "@microsoft/signalr";
-import { useMessages } from "./MessagesContext";
 
 
 function MessageContainer() {
     const chatId = '00000000-0000-0000-0000-000000000000'
 
     const dispatch = useDispatch()
-    const isLoggedIn = useSelector((state: RootState) => state.currentUser.isLoggedIn);
+    const isLoggedIn = useSelector((state: RootState) => state.currentUser.isLoggedIn)
     const isNotificationsEnabled = useSelector((state: RootState) => state.app.notificationsEnabled)
-    const hubState = useSelector((state: RootState) => state.signalr.connectionState);
-    const clearMessagesLoading = useSelector((state: RootState) => state.messages.clearMessagesLoading);
-    const messagesService = useMessages()
+    const hubState = useSelector((state: RootState) => state.signalr.connectionState)
+    const clearMessagesLoading = useSelector((state: RootState) => state.messages.clearMessagesLoading)
 
     useEffect(() => {
-        if (isLoggedIn && hubState === HubConnectionState.Connected) {
-            // should this be called every time the page is opened? this decision depends on if the hub should be disconnected when leaving the page
-            messagesService.fetchMessages()
+        if (isLoggedIn) {
             dispatch(getNotificationEnabled())
         }
-    }, [dispatch, isLoggedIn, messagesService, hubState]);
+    }, [dispatch, isLoggedIn])
 
     return (
         <>
